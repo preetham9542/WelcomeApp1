454d12d75495b3f38cd9cae77261c88a83079628
abc <abc@gmail.com>
2024-02-15 12:09:22 +0530

    #frst

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeApp1;
+
+Console.WriteLine("Hello, World! Welcome to Authentication App");
+Signin obj = new Signin();
+Console.WriteLine(obj.Authenticate("admin","admin"));
---
+++ Signin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp1
+{
+    public class Signin
+    {
+        public string Authenticate(string username, string password)
+        {
+            string msg;
+            if ((string.IsNullOrEmpty(username)) || string.IsNullOrEmpty(password))
+            {
+                msg = "details required";
+            }
+            else
+            {
+                if((username == "admin") && (password == "admin"))
+                {
+                    msg = "Authentication Passed";
+                }
+                else
+                {
+                    msg = "Authentication failed";
+                }
+
+            }
+            return msg;
+            
+        }
+    }
+}
